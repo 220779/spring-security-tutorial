8e7f727385c9ceeb13df2a5d9c342ad1a78ad55c
220779 <102145019+220779@users.noreply.github.com>
2023-10-22 12:14:07 +0300

    [add] initial commit

---
+++ src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $org.dailycodebuffer;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
