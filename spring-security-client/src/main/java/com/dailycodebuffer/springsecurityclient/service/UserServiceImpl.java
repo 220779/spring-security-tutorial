8e6168833bbf81a1f734288096c3cdcad5afb483
220779 <102145019+220779@users.noreply.github.com>
2023-10-22 16:58:56 +0300

    [add] User Service implementation (termination of  registration)

--- spring-security-client/src/main/java/com/dailycodebuffer/springsecurityclient/service/UserServiceImpl.java
+++ spring-security-client/src/main/java/com/dailycodebuffer/springsecurityclient/service/UserServiceImpl.java
@@ -1,9 +1,11 @@
 package com.dailycodebuffer.springsecurityclient.service;
 
+import com.dailycodebuffer.springsecurityclient.config.WebSecurityConfig;
 import com.dailycodebuffer.springsecurityclient.entity.User;
 import com.dailycodebuffer.springsecurityclient.model.UserModel;
 import com.dailycodebuffer.springsecurityclient.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 
@@ -13,6 +15,9 @@ public class UserServiceImpl implements UserService {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     @Override
     public User registerUser(UserModel userModel) {
         User user = new User();
@@ -20,7 +25,9 @@ public class UserServiceImpl implements UserService {
         user.setLastName(userModel.getLastName());
         user.setEmail(userModel.getEmail());
         user.setRole("User");
-        user.setPassword(userModel.getPassword());
-        return null;
+        user.setPassword(passwordEncoder.encode(userModel.getPassword()));
+
+        userRepository.save();     //userRepository.save(user);
+        return user;
     }
 }
